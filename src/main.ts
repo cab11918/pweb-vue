8c485c4b1267139fb8355f9fde185dee8d679a7e
yumh <yumh@shukun.net>
2022-05-02 17:05:16 +0800

    initiated

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import router from './router/index'
+import store from './store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.use(ElementPlus)
+app.mount('#app')
